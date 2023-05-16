3b4919d1ed662e21c11970f45034935d1a5c90d8
Ray <Chuang>
2023-05-16 22:21:50 +0800

    Signup_page to log_page

--- app/src/main/java/com/example/app/Signup_page.java
+++ app/src/main/java/com/example/app/Signup_page.java
@@ -3,6 +3,7 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -36,39 +37,29 @@ public class Signup_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
         //將使用者帳戶送入資料庫
-        sign_email=findViewById(R.id.Signup_email);
-        sign_password=findViewById(R.id.Signup_password);
-        sign_login_button=findViewById(R.id.signup_login);
-        View.OnClickListener onClickListener=new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread (new Runnable(){
-                    @Override
-                    public void run() {
-                        mySQLhandler.run();
-                        try {
-                            String email=sign_email.getText().toString();
-                            String password=sign_password.getText().toString();
-                            mySQLhandler.CreateUser(email,password);
-                        } catch (SQLException | ClassNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }).start();
-//                try {
-//                    String email=sign_email.getText().toString();
-//                    String password=sign_password.getText().toString();
-//                    mySQLhandler.CreateUser(email,password);
-//                } catch (SQLException | ClassNotFoundException e) {
-//                    throw new RuntimeException(e);
-//                }
-            }
-        };
-        sign_login_button.setOnClickListener(onClickListener);
+        sign_email = findViewById(R.id.Signup_email);
+        sign_password = findViewById(R.id.Signup_password);
+        sign_login_button = findViewById(R.id.signup_login);
 
 
     }
+    //將使用者帳戶送入資料庫
+    public void onClickBtn_Signup(View view) {
+        new Thread (new Runnable(){
+            @Override
+            public void run() {
+                mySQLhandler.run();
+                try {
+                    String email=sign_email.getText().toString();
+                    String password=sign_password.getText().toString();
+                    mySQLhandler.CreateUser(email,password);
 
-
-
+                } catch (SQLException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+        Intent intent = new Intent(Signup_page.this, Login_page.class );
+        startActivity(intent);
+    }
 }
